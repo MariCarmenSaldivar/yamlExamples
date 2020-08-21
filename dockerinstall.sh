56bed2c54742a1a9a0c75a6201b4e9bafb14c321
Maricarme Saldivar <maricarmen.saldivar@gmail.com>
2020-08-21 16:39:40 -0500

    Update dockerinstall.sh

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -5,7 +5,8 @@ sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 
 #Agregar llave Docker GPG 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
 #sudo apt-key fingerprint 0EBFCD88
 
