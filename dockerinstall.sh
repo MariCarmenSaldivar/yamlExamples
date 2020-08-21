0b1ad6df0d08a52da5567c394a38eb0528f94cb6
Maricarme Saldivar <maricarmen.saldivar@gmail.com>
2020-08-21 15:30:59 -0500

    Create dockerinstall.sh

---
+++ dockerinstall.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Configurar el repositorio, instalar paquetespara usar el repositorio por https
+
+sudo apt-get update
+sudo apt-get install \
+apt-transport-https \
+ca-certificates \
+curl \
+gnupg-agent \
+software-properties-common
+
+#Agregar llave Docker GPG 
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
+
+#Agregamos repositorio mas estable
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+#Instalar Docker Engine
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
